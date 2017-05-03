23bcbf036c503bed61bd1d86872655790cee6c06
Dellion <keryhtvgames@mail.ru>
2017-05-03 19:23:29 +0300

    TASK_1 Add hall model

--- Cinema/src/main/java/models/Hall.java
+++ Cinema/src/main/java/models/Hall.java
@@ -1,11 +1,38 @@
 package models;
 
-/**
- * 03.05.2017
- * Hall
- *
- * @author Sidikov Marsel (First Software Engineering Platform)
- * @version v1.0
- */
 public class Hall {
+    private int HallId;
+    private String name;
+    private int HallPlacesNum[];
+
+    public Hall(String name, int HallPlaces) {
+        this.name = name;
+        this.HallPlacesNum = HallPlacesNum;
+    }
+    public Hall(int HallId, String name, int hallPlacesNum) {
+        this(name, hallPlacesNum);
+        this.HallId = HallId;
+    }
+    public int getHallId(){
+        return HallId;
+    }
+    public void setHallId(int hallId){
+        this.HallId = hallId;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getHallPlacesNum(){
+        return HallPlacesNum[getHallPlacesNum()];
+    }
+    public void setHallPlacesNum(int hallPlacesNum[]){
+        this.HallPlacesNum = hallPlacesNum;
+    }
+    @Override
+    public String toString(){
+        return HallId + " " + this.name + " " + this.HallPlacesNum;
+    }
 }
