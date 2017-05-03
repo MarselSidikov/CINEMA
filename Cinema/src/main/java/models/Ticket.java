304e6d771bfba72033ceb79c757981cb58eff065
MaximMarshalov <marsandnats@yandex.ru>
2017-05-03 19:22:58 +0300

    TASK_1 Add ticket model

--- Cinema/src/main/java/models/Ticket.java
+++ Cinema/src/main/java/models/Ticket.java
@@ -1,5 +1,7 @@
 package models;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
 /**
  * 03.05.2017
  * Ticket
@@ -8,4 +10,59 @@ package models;
  * @version v1.0
  */
 public class Ticket {
+     private int id;
+     private User user;
+    private Seance seance;
+    private int place;
+
+    public Ticket(){}
+
+    public Ticket(int id, User user, Seance seance, int place) {
+        this.id = id;
+        this.user = user;
+        this.seance = seance;
+        this.place = place;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Seance getSeance() {
+        return seance;
+    }
+
+    public void setSeance(Seance seance) {
+        this.seance = seance;
+    }
+
+    public int getPlace() {
+        return place;
+    }
+
+    public void setPlace(int place) {
+        this.place = place;
+    }
+
+    public String toString() {
+        return "Ticket{" +
+                "id:" + id +
+                ", user:" + user +
+                ", seance:" + seance +
+                ", place:" + place +
+                '}';
+    }
+   
 }
