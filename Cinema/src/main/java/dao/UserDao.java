0579b6f83a2ff335818ddc3b61fc7b5f9a3ad2fa
Sergey <tikhomirov2000@inbox.ru>
2017-05-03 23:01:10 +0300

    TASK_2 add UserDao

---
+++ Cinema/src/main/java/dao/UserDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import models.User;
+
+/**
+ * 03.05.2017
+ * UserDao
+ *
+ * @author Tikhomirov Sergey (ITIS)
+ * @version v1.0
+ */
+public interface UserDao extends BaseDao <User> {
+
+}
