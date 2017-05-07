7d059a9b72be2f0068c6f46e47dc34bb34b7b277
рафаиль шига < >
2017-05-07 20:09:51 +0300

    TASK_2 add CinemaDao

--- Cinema/src/main/java/dao/CinemaDao.java
+++ Cinema/src/main/java/dao/CinemaDao.java
@@ -3,7 +3,11 @@ package dao;
 import models.Cinema;
 
 /**
- * Created by acer on 07.05.2017.
+ * 03.05.2017
+ * Cinema
+ *
+ * @author Marat Shigabutdinov (ITIS)
+ * @version v1.0
  */
 public interface CinemaDao extends BaseDao<Cinema> {
 }
